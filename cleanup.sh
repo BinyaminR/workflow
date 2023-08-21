0b8e780fb1e2162433665e2946dfaa2bebd801f4
BinyaminR <binyamin.rothenberg@gmail.com>
2023-08-21 09:52:39 +0300

    4

---
+++ cleanup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define the directory to clean up
+directory="/path/to/your/directory"
+
+# Define the maximum age (in days) for files to be deleted
+max_age_days=30
+
+# Calculate the date threshold (30 days ago)
+threshold_date=$(date -d "-${max_age_days} days" +%s)
+
+# Iterate through files in the directory and delete old ones
+for file in "$directory"/*; do
+    file_mtime=$(stat -c %Y "$file")
+    if (( file_mtime < threshold_date )); then
+        rm "$file"
+    fi
+done
